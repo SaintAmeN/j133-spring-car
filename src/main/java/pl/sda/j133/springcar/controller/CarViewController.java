91dbe203b557df3ea9a55ccc45a28f47cd3d82bd
Paweł Recław <pawel.reclaw@gmail.com>
2023-03-05 15:46:37 +0100

    Formularz samochodu

--- src/main/java/pl/sda/j133/springcar/controller/CarViewController.java
+++ src/main/java/pl/sda/j133/springcar/controller/CarViewController.java
@@ -3,8 +3,10 @@ package pl.sda.j133.springcar.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.sda.j133.springcar.model.dto.CarResponse;
+import pl.sda.j133.springcar.model.dto.CreateCarRequest;
 import pl.sda.j133.springcar.service.CarService;
 
 import java.util.List;
@@ -32,5 +34,22 @@ public class CarViewController {
         model.addAttribute("atrybutListaSamochodow", listaSamochodow);
 
         return "car-list-page";
+    }
+
+    // http://localhost:8080/view/car/form
+    //
+    // formularz wysyła => POST
+    // przegladarka zawsze działa na GET
+    // JavaScript umożliwia wywoływanie PATCH,PUT,DELETE
+    @GetMapping("/form")
+    public String pobierzStroneFormularza(Model model) {
+        model.addAttribute("atrybutObiektFormularza", new CreateCarRequest());
+        return "car-form-page";
+    }
+
+    @PostMapping("/form")
+    public String przeslijWypelnionyFormularz(Model model, CreateCarRequest request) {
+        carService.add(request);
+        return "redirect:/view/car";
     }
 }
